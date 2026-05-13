2346af63a53cc407987e6ab70c3713f3a2485375
Mei Torres <mei.torres@opendeusto.es>
2026-05-13 20:03:44 +0200

    tests corregidos

--- tests/test_bruteforce.py
+++ tests/test_bruteforce.py
@@ -1,12 +1,13 @@
 from password_cracker.attacks.brute_force import brute_force_attack
 
 def test_mask():
-    hash_target = "5f4dcc3b5aa765d61d8327deb882cf99"
+
+    hash_target = "5d41402abc4b2a76b9719d911017c592"  # "hello"
 
     result, attempts = brute_force_attack(
         hash_target,
-        "?l?l?l?l?l?l",
+        "?l?l?l?l?l",
         "md5"
     )
 
-    assert result is not None
+    assert result == "hello"
--- tests/test_hashing.py
+++ tests/test_hashing.py
@@ -1,4 +1,4 @@
 from password_cracker.utils.hashing import hash_text
 
 def test_sha1():
-    assert hash_text("hola", "sha1") == "f7ff9e8b7bb7c... (hash correcto)"
+    assert hash_text("hola", "sha1") == "99800b85d3383e3a2fb45eb7d0066a4879a9dad0"
